9391a8b7fe2601f0880e9c21f21acfe09a592f46
Armaritto <armia.joseph35@gmail.com>
2023-12-19 19:08:02 +0200

    Initial commit

---
+++ src/test/java/com/berrymail/BerryMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berrymail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerryMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
