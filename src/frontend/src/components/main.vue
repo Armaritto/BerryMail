6f0ce63d5c72cea1d5abad9fee6d8f48994515b6
Armaritto <armia.joseph35@gmail.com>
2023-12-25 19:36:39 +0200

    search - filter
    Co-Authored-By: PierreJack <111060450+SantiagoMontag@users.noreply.github.com>

--- src/frontend/src/components/main.vue
+++ src/frontend/src/components/main.vue
@@ -1,5 +1,4 @@
 <template>
-
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
   <br>
   <br>
@@ -17,10 +16,10 @@
           <div class="form-row mb-3">
             <label for="Priority" class="col-2 col-sm-1 col-form-label">Priority:</label>
             <div class="Priority-group">
-              <button onclick="" class="btn btn-priority">Critical</button>
-              <button type="submit" class="btn btn-priority">Urgent</button>
-              <button type="submit" class="btn btn-priority">Moderate</button>
-              <button type="submit" class="btn btn-priority">Default</button>
+              <button @click="handlePriority(isCritical)" :class="{ 'true': this.isCritical.value, 'false': !this.isCritical.value }" class="btn btn-priority">Critical</button>
+              <button @click="handlePriority(isUrgent)"  :class="{ 'true': this.isUrgent.value, 'false': !this.isUrgent.value }" type="submit" class="btn btn-priority">Urgent</button>
+              <button @click="handlePriority(isModerate)" :class="{ 'true': this.isModerate.value, 'false': !this.isModerate.value }" type="submit" class="btn btn-priority">Moderate</button>
+              <button @click="handlePriority(isDefault)" :class="{ 'true': this.isDefault.value, 'false': !this.isDefault.value }" type="submit" class="btn btn-priority">Default</button>
             </div>
           </div>
           <div class="form-row mb-3">
@@ -45,21 +44,7 @@
         </div>
       </main>
     </div>
-
   </div>
-  <div style="display: flex; justify-content: center;position: absolute;top: 25%; left: 50%">
-    <form>
-      <h3>Login</h3>
-      <label for="email">Email</label>
-      <input type="text" placeholder="example@berry.com" id="username">
-
-      <label for="password">Password</label>
-      <input type="password" placeholder="Password" id="password">
-      <button>Log In</button>
-      <button>Create Account</button>
-    </form>
-  </div>
-  </body>
 </template>
 
 <script>
@@ -67,12 +52,37 @@ export default {
   name: 'main',
   props: {
     msg: String
-  }
+  },
+  data(){
+    return{
+      isCritical:{value:false},
+      isUrgent:{value:false},
+      isModerate:{value:false},
+      isDefault:{value:true}
+      }
+    },
+    methods:{
+      handlePriority(priority){
+        this.isCritical.value = false;
+        this.isUrgent.value = false;
+        this.isModerate.value = false;
+        this.isDefault.value = false;
+        priority.value = true;
+      }
+    }
 }
 </script>
 
-
 <style>
+.btn-priority.true {
+  background-color: #521bb4;
+  color: white;
+}
+
+.btn-priority.false {
+  background-color: rgba(255, 255, 255, 0.44);
+  color: white;
+}
 .priority-group{
   display: flex;
   flex-direction: row;
@@ -387,119 +397,4 @@ h3{
   cursor: pointer;
 }
 @import'~bootstrap/dist/css/bootstrap.css';
-=======
-<style scoped>
-*,
-*:before,
-*:after{
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-body{
-  background-color: #080710;
-}
-
-.background{
-  width: 430px;
-  height: 520px;
-  position: absolute;
-  transform: translate(-50%,-50%);
-  left: 50%;
-  top: 50%;
-}
-.background .shape{
-  height: 200px;
-  width: 200px;
-  position: absolute;
-  border-radius: 50%;
-}
-
-form{
-  height: 520px;
-  width: 400px;
-  background-color: rgba(44, 19, 66, 0.13);
-  position: absolute;
-  border-radius: 10px;
-  backdrop-filter: blur(10px);
-  border: 2px solid  rgba(44, 19, 66, 0.13);
-  box-shadow: 0 0 40px rgba(8,7,16,0.6);
-  padding: 50px 35px;
-}
-
-form *{
-  font-family: 'Poppins',sans-serif;
-  color: #ffffff;
-  letter-spacing: 0.5px;
-  outline: none;
-  border: none;
-}
-form h3{
-  font-size: 32px;
-  font-weight: 500;
-  line-height: 42px;
-  text-align: center;
-}
-label{
-  display: block;
-  margin-top: 30px;
-  font-size: 16px;
-  font-weight: 500;
-}
-input{
-  display: block;
-  height: 50px;
-  width: 100%;
-  background-color: rgba(255,255,255,0.07);
-  border-radius: 3px;
-  padding: 0 10px;
-  margin-top: 8px;
-  font-size: 14px;
-  font-weight: 300;
-}
-::placeholder{
-  color: #e5e5e5;
-}
-button{
-  margin-top: 50px;
-  width: 100%;
-  background-color: #ffffff;
-  color: #080710;
-  padding: 15px 0;
-  font-size: 18px;
-  font-weight: 600;
-  border-radius: 5px;
-  cursor: pointer;
-}
-button:last-child{
-  background-color: rgba(255,255,255,0.07);
-  background-color: #bc3fdb;
-  margin-top: 20px;
-}
-button:hover{
-  background-color: #521bb4;
-  color: #080710;
-}
-.social{
-  margin-top: 30px;
-  display: flex;
-}
-.social div{
-  width: 150px;
-  border-radius: 3px;
-  padding: 5px 10px 10px 5px;
-  background-color: rgba(255,255,255,0.27);
-  color: #eaf0fb;
-  text-align: center;
-}
-.social div:hover{
-  background-color: rgba(255,255,255,0.47);
-}
-.social .fb{
-  margin-left: 25px;
-}
-.social i{
-  margin-right: 4px;
-}
-
 </style>
