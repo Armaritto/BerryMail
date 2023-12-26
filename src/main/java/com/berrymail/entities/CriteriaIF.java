8f12974229130078cd07d904e1b0fba918dfc9f2
PierreJack <111060450+SantiagoMontag@users.noreply.github.com>
2023-12-26 04:50:38 +0200

    .

--- src/main/java/com/berrymail/entities/CriteriaIF.java
+++
@@ -1,5 +0,0 @@
-package com.berrymail.entities;
-import java.util.ArrayList;
-public interface CriteriaIF {
-    public ArrayList<Mail> meetCriteria(ArrayList<Mail> mails, ArrayList<String> criteria);
-}
